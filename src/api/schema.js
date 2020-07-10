09850947f28bd5ef7714860f4a656a0b1c6a3238
Hiago Prates <hiagohps360@gmail.com>
2020-07-10 15:20:31 -0300

    add schemas

---
+++ src/api/schema.js
@@ -0,0 +1,30 @@
+const resolver = require('./resolver');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const usersAttribs = `
+  id: ID
+  name: String!
+  email: String!
+  password: String!
+`;
+
+const typeDefs = `
+  type User {
+    ${usersAttribs}
+  }
+
+  type Query {
+    getUser(id: ID!): User
+    getUsers: [User]
+  }
+
+  input UserInput {
+    ${usersAttribs}
+  }
+
+  type Mutation {
+    createUser(input: UserInput): User
+  }
+`;
+
+module.exports = makeExecutableSchema({ typeDefs, resolver })
